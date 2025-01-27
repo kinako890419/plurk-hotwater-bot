bffc2e38a7dcbe91677c77a6b8d22aedc559ab56
kinako890419 <kinako890419@gmail.com>
2025-01-27 15:30:51 +0800

    修正錯誤

--- src/bot.py
+++ src/bot.py
@@ -2,8 +2,8 @@ import logging
 from config import load_config
 from plurk_oauth import PlurkAPI
 from plurk_response import PlurkResponse
-from plurk_daily_post import DailyPost 
-
+from plurk_daily_post import DailyPost
+from response_content import ContentResponse
 
 # 設定日誌
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -12,6 +12,7 @@ def main():
     logging.info("載入配置")
     config = load_config()
     plurk_api_key = config['PLURK']
+    gemini_api_key = config['GEMINI']['api_key']
 
     plurk_api = PlurkAPI(
         plurk_api_key['consumer_key'],
@@ -22,7 +23,8 @@ def main():
         plurk_api_key['token_secret']
     )
 
-    bot = PlurkResponse(plurk_api)
+    content_response = ContentResponse(gemini_api_key)
+    bot = PlurkResponse(plurk_api, content_response)
     daily_post = DailyPost(plurk_api)
 
     logging.info("啟動 PlurkBot")
